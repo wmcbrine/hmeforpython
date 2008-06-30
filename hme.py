2c95344f6997ba2514a628d3117da9d37e355fe4
William McBrine <wmcbrine@gmail.com>
2008-06-30 02:02:38 -0400

    I really shouldn't encourage "from hme import *".

--- hme.py
+++ hme.py
@@ -16,26 +16,26 @@
 
 """ HME for Python
 
-    An implementation of TiVo's HME ("Home Media Extensions") Protocol 
-    for Python. This is based on the protocol specification, but it is 
+    An implementation of TiVo's HME ("Home Media Extensions") Protocol
+    for Python. This is based on the protocol specification, but it is
     not a port of the Java SDK, and it does things somewhat differently.
-    (Starting from the spec, I did end up with a lot of similarities to 
-    the SDK, and I then tweaked this module to bring them closer, in 
+    (Starting from the spec, I did end up with a lot of similarities to
+    the SDK, and I then tweaked this module to bring them closer, in
     some respects; but there are still a lot of differences.)
 
-    Basic usage: import hme (or more likely, "from hme import *"); 
-    subclass the "Application" class; and override some of the stub 
-    functions that appear at the end of this file. (startup() and 
-    handle_key_press() are the most useful.) If you want to use it with 
-    the included hmeserver.py, then your program should be in the form 
-    of a module (see the included examples). Aside from the app class, 
-    you may want to include TITLE and/or CLASS_NAME strings; TITLE is 
-    the display title, and CLASS_NAME is the name of your main app 
-    class. (Both will be derived from the module name if absent.)
+    Basic usage: import hme (or "from hme import *"); subclass the
+    "Application" class; and override some of the stub functions that
+    appear at the end of this file. (startup() and handle_key_press()
+    are the most useful.) If you want to use it with the included
+    hmeserver.py, then your program should be in the form of a module
+    (see the included examples). Aside from the app class, you may want
+    to include TITLE and/or CLASS_NAME strings; TITLE is the display
+    title, and CLASS_NAME is the name of your main app class. (Both will
+    be derived from the module name if absent.)
 
-    startup() is called first, after the initial events are handled; 
+    startup() is called first, after the initial events are handled;
     then the event loop runs until either it's out of events (i.e. the
-    socket closed), or you set self.active to False. Finally, cleanup() 
+    socket closed), or you set self.active to False. Finally, cleanup()
     is called.
 
     Items not yet implemented from the spec:
